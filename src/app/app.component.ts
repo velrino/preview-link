b9d869a0f3f7ed420c75491e564e07e0031ec8aa
velrino <denismagalhaesvelrino@gmail.com>
2021-06-09 12:20:09 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, VERSION } from '@angular/core';
+import { getLinkPreview, getPreviewFromContent } from 'link-preview-js';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    getLinkPreview('http://www.youtube.com/watch?v=MejbOFk7H6c', {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).then(data => console.debug(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
